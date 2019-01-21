3d2a7514ecfa0e8b3ba70bab13bf7c1fb6a3fb39
Daniel Ramos <danielramosacosta@hotmail.com>
2019-01-21 08:59:50 +0000

    First commit

---
+++ Sistema.Web/Controllers/CategoriasController.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Sistema.Datos;
+using Sistema.Entidades.Almacen;
+using Sistema.Web.Models.Almacen.Categoria;
+
+namespace Sistema.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriasController : ControllerBase
+    {
+        private readonly DbContextSistema _context;
+
+        public CategoriasController(DbContextSistema context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Categorias/List
+        [HttpGet("[action]")]
+        public async Task<IEnumerable<CategoriaViewModel>> List()
+        {
+            var categoria = await _context.Categorias.ToListAsync();
+
+            return categoria.Select(c => new CategoriaViewModel
+            {
+                idcategoria = c.idcategoria,
+                nombre = c.nombre,
+                descripcion = c.descripcion,
+                condicion = c.condicion
+            });
+        }
+
+        // GET: api/Categorias/Show/5
+        [HttpGet("[action]/{id}")]
+        public async Task<IActionResult> Show([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var categoria = await _context.Categorias.FindAsync(id);
+
+            if (categoria == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(new CategoriaViewModel
+            {
+                idcategoria = categoria.idcategoria,
+                nombre = categoria.nombre,
+                descripcion = categoria.descripcion,
+                condicion = categoria.condicion
+            });
+        }
+
+        // PUT: api/Categorias/Update/5
+        [HttpPut("[action]/{id}")]
+        public async Task<IActionResult> Update([FromRoute] int id, [FromBody] CategoriaUpdateViewModel categoriaViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != categoriaViewModel.idcategoria)
+            {
+                return BadRequest();
+            }
+
+            // _context.Entry(categoria).State = EntityState.Modified;
+            var categoria = await _context.Categorias.FirstOrDefaultAsync(c => c.idcategoria == categoriaViewModel.idcategoria);
+
+            if (categoria == null)
+            {
+                return NotFound();
+            }
+
+            categoria.nombre = categoriaViewModel.nombre;
+            categoria.descripcion = categoriaViewModel.descripcion;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CategoriaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Categorias/Create
+        [HttpPost("[action]")]
+        public async Task<IActionResult> Create([FromBody] CategoriaCreateViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            Categoria categoria = new Categoria
+            {
+                nombre = model.nombre,
+                descripcion = model.descripcion,
+                condicion = true
+            };
+
+            _context.Categorias.Add(categoria);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCategoria", new { id = categoria.idcategoria }, categoria);
+        }
+
+        // DELETE: api/Categorias/Delete/5
+        [HttpDelete("[action]/{id}")]
+        public async Task<IActionResult> Delete([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var categoria = await _context.Categorias.FindAsync(id);
+            if (categoria == null)
+            {
+                return NotFound();
+            }
+
+            _context.Categorias.Remove(categoria);
+            await _context.SaveChangesAsync();
+
+            return Ok(categoria);
+        }
+
+        // PUT: api/Categorias/Disable/5
+        [HttpPut("[action]/{id}")]
+        public async Task<IActionResult> Disable([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            // _context.Entry(categoria).State = EntityState.Modified;
+            var categoria = await _context.Categorias.FirstOrDefaultAsync(c => c.idcategoria == id);
+
+            if (categoria == null)
+            {
+                return NotFound();
+            }
+
+            categoria.condicion = true;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // PUT: api/Categorias/Enable/5
+        [HttpPut("[action]/{id}")]
+        public async Task<IActionResult> Enable([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            // _context.Entry(categoria).State = EntityState.Modified;
+            var categoria = await _context.Categorias.FirstOrDefaultAsync(c => c.idcategoria == id);
+
+            if (categoria == null)
+            {
+                return NotFound();
+            }
+
+            categoria.condicion = false;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CategoriaExists(int id)
+        {
+            return _context.Categorias.Any(e => e.idcategoria == id);
+        }
+    }
+}
---
+++ Sistema.Web/Models/Almacen/Categoria/CategoriaCreateViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sistema.Web.Models.Almacen.Categoria
+{
+    public class CategoriaCreateViewModel
+    {
+        [Required]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "El nombre no debe tener más de 50 caracteres ni menos de 3")]
+        public string nombre { get; set; }
+        [StringLength(256)]
+        public string descripcion { get; set; }
+    }
+}
